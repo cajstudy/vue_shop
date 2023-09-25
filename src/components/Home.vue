b39352791347ecd12db90058e75b0cd21812be3f
cajstudy <3100966836@qq.com>
2023-09-25 21:49:50 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div >
+
+
+      <el-container>
+        <!-- 头部区域 -->
+        <el-header>Header
+           <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <!-- 页面主体区 -->
+        <el-container>
+          <!-- 页面侧边栏 -->
+          <el-aside width="200px">Aside</el-aside>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    logout(){
+      window.sessionStorage.clear();
+      this.$router.push('/login')
+    }
+
+  }
+ }
+</script>
+
+<style lang="less" scoped>
+.el-header{
+background-color: #8B4726;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css';
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://1.13.159.135/api/private/v1/'
+Vue.prototype.$http = axios
+
+// 导入全局样式表
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
